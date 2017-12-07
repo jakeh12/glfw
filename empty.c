29c1900b740799d9df7473c2650a0972f899d113
Jakub Hladik <jhladik@iastate.edu>
2017-12-07 14:01:09 -0600

    initial commit

---
+++ empty.c
@@ -0,0 +1,73 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+  GLFWwindow* window = NULL;
+  const GLubyte* renderer;
+  const GLubyte* version;
+
+  glfwSetErrorCallback(error_callback);
+ 
+  if (!glfwInit()) {
+    fprintf(stderr, "ERROR: could not start GLFW3\n");
+    return 1;
+  }
+ 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+ 
+  window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+  if (!window) {
+    fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+    glfwTerminate();
+    return 1;
+  }
+  
+  glfwSetKeyCallback(window, key_callback);
+
+  glfwMakeContextCurrent(window);
+ 
+  /* start GLEW extension handler */
+  glewExperimental = GL_TRUE;
+  glewInit();
+ 
+  /* get version info */
+  renderer = glGetString(GL_RENDERER);
+  version = glGetString(GL_VERSION);
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported: %s\n", version);
+ 
+  /* tell GL to only draw onto a pixel if the shape is closer to the viewer */
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+ 
+  /* OTHER STUFF GOES HERE NEXT */
+  while(!glfwWindowShouldClose(window)) {
+      int width, height;
+      glfwGetFramebufferSize(window, &width, &height);
+      glViewport(0, 0, width, height);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+  }
+  /* close GL context and any other GLFW resources */
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
+
